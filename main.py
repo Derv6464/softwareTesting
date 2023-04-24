0579cd87d08653c7e2ab1e66164dd1081c82f5ec
Derv6464 <dervlag6464@gmail.com>
2023-04-24 15:18:14 +0100

    made basic rooms and a display function

---
+++ main.py
@@ -0,0 +1,8 @@
+rooms = ["meeting", "moon", "food", "young kids", "old kids", "adults", "seniors", "all ages"]
+
+def displayAllRooms(rooms):
+    for room in rooms:
+        print(room + " room")
+
+displayAllRooms(rooms)
+
