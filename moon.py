175eaca27ae70bacdb8db8b4622c6b6c4e8897c3
AmyMcMahon <89188326+AmyMcMahon@users.noreply.github.com>
2023-04-28 11:49:25 +0100

    Delete moon.py

--- moon.py
+++
@@ -1,7 +0,0 @@
-from fullmoon import NextFullMoon
-from fullmoon import IsFullMoon
-import datetime
-i = IsFullMoon()
-date = str((datetime.datetime.now()).date())
-print(date)
-print(i.set_date_string(date, '%Y-%m-%d').is_full_moon())
