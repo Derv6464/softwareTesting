6b0879732b65d10115c28749faf02b44c293ff77
Derv6464 <dervlag6464@gmail.com>
2023-04-30 19:45:45 +0100

    fixed userBooked, it works now

--- constaints.py
+++ constaints.py
@@ -30,7 +30,7 @@ class Room:
         self.maxAge = maxAge
         self.minAge = minAge
 
-allTimes = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"]
+allTimes = ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"]
 allDates = [(datetime.datetime.today() + datetime.timedelta(days=x)).strftime("%x") for x in range(7)]
 allRooms = [Room("Meeting", 200, 65, 18), Room("Moon", 20, 46, 26), Room("Food", 15, 100, 1), Room("Young Kids", 30, 12, 0), Room("Old Kids", 15, 18, 12), Room("Adults", 50, 64, 18), Room("Seniors", 65, 100, 65), Room("All Ages", 160, 100, 0)]
 
@@ -64,7 +64,7 @@ def form1Checks(booking):
 def form2Checks(booking):
     errorMSG = "You can't: "
     passes = True
-    if not userBooked(booking[5], booking[6], booking[1], booking[2], bookings):
+    if not userBooked(booking[5], booking[6], booking[1], booking[7], bookings):
         errorMSG += "You already have a booking at this time"
         passes = False
     return [passes, errorMSG]
@@ -100,12 +100,12 @@ def getAvabileTimes(date, room, length ,bookings):
     for i in bookings:
         if i[1] == str(date) and i[0] == room.name:
             daysBookings.append(i)
-    print(type(bookings[0][7]))
+    
     if not daysBookings:
         return allTimes
     avaTimes = []
     addTime = True
-    
+
     if meetLength ==1 :
         for i in allTimes:
             for j in daysBookings:
@@ -114,7 +114,6 @@ def getAvabileTimes(date, room, length ,bookings):
             if addTime:
                 avaTimes.append(i)
             addTime = True
-    #adding 1 ]/2 prob wont work cause date is a string in the csv
     elif meetLength == 2:
             for i in allTimes:
                 for j in daysBookings:
@@ -130,13 +129,10 @@ def getAvabileTimes(date, room, length ,bookings):
                     addTime = False
             if addTime:
                 avaTimes.append(i)
-    print(avaTimes)
     return avaTimes
 
 def addBooking(booking):
-    #include id if we do id, and make booking refrence
     d = open("bookings.csv", 'a')
-    
     for i in range(int(booking[3][0])):
         d.write( "\n")
         d.write(str(booking[0].name) + "," + str(booking[1]) + "," + str(booking[2]) + "," + str(booking[3]) + "," + str(booking[4]) + "," + str(booking[5]) + "," + str(booking[6]+","+str((datetime.datetime.strptime(booking[7],'%H:%M') + datetime.timedelta(hours=i)).strftime('%H:%M'))))
@@ -152,8 +148,12 @@ def checkValidId(id):
 #csv order = Room,Date,Time,Age,Length,userID,bookingRef
 #must be checked after second form is submited
 def userBooked(name, phone, date, time, bookings) :
+    print(name, phone, date, time)
+
     for booking in bookings:
-        if name == booking[5] and phone == booking[6] and date == booking[1] and time == booking[7]:
+        print(booking[5], booking[6], booking[1], booking[7])
+        if name == booking[5] and phone == booking[6] and date.strftime("%Y-%m-%d") == booking[1] and time == booking[7]:
+            print('NO BOOKING')
             return False
     return True
 
@@ -188,7 +188,6 @@ def checkHoliday(date):
     month = date.month
     year = date.year
     response = requests.get(url, params={"api_key": api_key, "country": country, "year": year, "month": month, "day": day})
-    print("holiday api:" + response.text)
     if response.text == "[]" or response.text == '{"error":{"message":"You have exceeded the requests per second allowed by your current plan. Visit the Abstract dashboard to upgrade for a higher limit.","code":"too_many_requests","details":null}}': 
         return True
     else:
